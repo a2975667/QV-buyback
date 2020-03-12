15c077ef557c7b4605b662832cb849422056156d
Ti-Chung Cheng <tcheng10@illinois.edu>
2020-03-12 12:14:51 -0500

    polish app ready for pilot
    Former-commit-id: cdacce49a1dd109f15c2a5311a385b7522ebb4bd

--- server/public/main-es5.js
+++ server/public/main-es5.js
@@ -3982,7 +3982,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
     var environment = {
       production: false,
-      apiUrl: 'http://localhost:5000'
+      apiUrl: ''
     };
     /*
      * For easier debugging in development mode, you can import the following file
